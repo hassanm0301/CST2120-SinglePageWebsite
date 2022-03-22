fa23d90eaf6370a9aed6a00fd0612f94cb15e763
Hassan Maudarbocus <Hassanm0301@gmail.com>
2022-03-22 17:49:53 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import fetch from 'node-fetch';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(cors({
+    origin: 'https://api.steampowered.com/',
+    methods: "GET,PUT,POST,DELETE"
+}));
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("App listening at http://%s:%s", host, port);
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd()+'/page.html');
+})
+
+app.get('/getAppList', async function (req, res) {
+    let response = await fetch("https://api.steampowered.com/ISteamApps/GetAppList/v2/");
+    if (response.ok) {
+        let json = await response.json();
+        res.send(json);
+      } else {
+        res.send("HTTP-Error: " + response.status);
+      }
+})
