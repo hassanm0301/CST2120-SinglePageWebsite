1a8652ae3cc86ce91521fa2705b915cdbbb9a9be
Hassan Maudarbocus <Hassanm0301@gmail.com>
2022-04-01 15:49:51 +0400

    added searching and displaying of searches

--- public/search.js
+++ public/search.js
@@ -13,7 +13,27 @@ $(document).ready(() => {
         dataType: "json",
         encode: true,
       }).done((data) => {
-        console.log(data);
+        data.forEach(element => {
+            createGameBox(element)
+        });
       });
     });
 });
+
+function createGameBox(gameData){
+    let parentNode = $("#searchResults");
+    let childNode = `
+    <div id="game${gameData.game_id}">
+        <h2>${gameData.Name}</h2>
+        <div class="detailBox">
+            <p>
+                Number of reviews ${gameData.rating_number}<br>
+                Rating ${gameData.ave_rating}<br>
+                Description ${gameData.Description}<br>
+            </p>
+        </div>
+        <img src="/img/down.png" alt="downArrow" onclick="displayReview('${gameData.game_id}')" height="50" width="50">
+    </div>
+    `;
+    parentNode.append(childNode);
+}
